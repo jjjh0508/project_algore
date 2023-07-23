7b70b5fd8ba6043859ab19ecd047427bd2710af3
JIHWAN <jjjh0508@gmail.com>
2023-07-24 00:55:41 +0900

    BUG: 검색 문제 해결

--- src/main/java/com/algore/application/search/dao/SearchMapper.java
+++ src/main/java/com/algore/application/search/dao/SearchMapper.java
@@ -3,12 +3,13 @@ package com.algore.application.search.dao;
 import com.algore.application.home.dto.MainViewDTO;
 import com.algore.application.search.dto.SearchDTO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 @Mapper
 public interface SearchMapper {
-    List<SearchDTO> searchRecipe(String recipeName,int startIndex ,int pageSiz,int page);
+    List<SearchDTO> searchRecipe(@Param("recipeName") String recipeName,@Param("startIndex")int startIndex , @Param("pageSiz")int pageSiz, @Param("page")int page);
 
     int searchCnt(String recipeName);
 }
